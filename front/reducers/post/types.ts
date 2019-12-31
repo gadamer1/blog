a65f151d159d080f8ef96118b6b008cf4daf395f
gadamer1 <dngmlws21@korea.ac.kr>
2019-12-31 18:27:48 +0900

    유저 리듀서 구현 + 게시글 작성부분 덜 구현됨

---
+++ front/reducers/post/types.ts
@@ -0,0 +1,78 @@
+import {
+  MAKE_POST_FAILURE,
+  MAKE_POST_REQUEST,
+  MAKE_POST_SUCCESS,
+  GET_POSTS_REQUEST,
+  GET_POSTS_SUCCESS,
+  GET_POSTS_FAILURE
+} from "./actions";
+// STATES
+
+/*post STATE */
+export interface post {
+  nickname: string;
+}
+
+/* 로딩 상태 */
+export interface loadingStates {
+  isPosting: boolean;
+  isPostsLoading: boolean;
+  isPostLoading: boolean;
+}
+
+/* meta states */
+export interface metaStates {
+  postStautsCode: number;
+  getPostsStatusCode: number;
+  getPostStatusCode: number;
+}
+export interface postStore {
+  posts: post[] | null;
+  currentPost: post | null;
+  loadingStates: loadingStates;
+  metaStates: metaStates;
+}
+
+// ACTIONS
+
+/* 포스팅 행위 */
+export interface makePostRequestAction {
+  type: typeof MAKE_POST_REQUEST;
+  payload: {
+    email: string;
+    password: string;
+  };
+}
+export interface makePostSuccessAction {
+  type: typeof MAKE_POST_SUCCESS;
+  result: post;
+}
+export interface makePostFailureAction {
+  type: typeof MAKE_POST_FAILURE;
+  errorCode: number;
+}
+
+/* 게시글들 가져오기 */
+export interface getPostsRequsetAction {
+  type: typeof GET_POSTS_REQUEST;
+  payload: {
+    author?: string;
+    pageNumber: number;
+  };
+}
+export interface getPostsSuccessAction {
+  type: typeof GET_POSTS_SUCCESS;
+  result: post[];
+}
+export interface getPostsFailureAction {
+  type: typeof GET_POSTS_FAILURE;
+  errorCode: number;
+}
+
+export type userActions =
+  | makePostRequestAction
+  | makePostSuccessAction
+  | makePostFailureAction
+  | getPostsRequsetAction
+  | getPostsSuccessAction
+  | getPostsFailureAction;
--- front/sagas/user.ts
+++ front/sagas/user.ts
@@ -1,7 +1,72 @@
-import { all, fork } from "redux-saga/effects";
+import { all, fork, put, takeLatest, call } from "redux-saga/effects";
+import axios from "axios";
+import {
+  loginRequestAction,
+  signUpRequsetAction
+} from "../reducers/user/types";
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE
+} from "../reducers/user/actions";
 
-function* watchLogin() {}
+// 회원가입 요청
+function signUpAPI(data) {
+  return axios.post("/user", data, {
+    withCredentials: true
+  });
+}
+
+function* signUp(action: signUpRequsetAction) {
+  try {
+    const User = yield call(signUpAPI, action.payload);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+      result: User.data
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: e.status
+    });
+  }
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
+}
+
+// 로그인 요청
+function loginAPI(data) {
+  return axios.post("/auth", data, {
+    withCredentials: true
+  });
+}
+
+function* login(action: loginRequestAction) {
+  try {
+    const User = yield call(loginAPI, action.payload);
+    yield put({
+      type: LOGIN_SUCCESS,
+      result: User.data
+    });
+  } catch (e) {
+    console.error(e);
+    yield put({
+      type: LOGIN_FAILURE,
+      error: e.status
+    });
+  }
+}
+
+function* watchLogin() {
+  yield takeLatest(LOGIN_REQUEST, login);
+}
 
 export default function* userSaga() {
-  yield all([fork(watchLogin)]);
+  yield all([fork(watchLogin), fork(watchSignUp)]);
 }
